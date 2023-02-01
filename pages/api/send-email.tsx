4c89ee40527b3e8e5b343ca239da3ef4f0c54cd8
Larry Chiem <larrychiem@gmail.com>
2023-02-01 01:28:15 -0800

    Fixed Potiental issues

--- pages/api/send-email.tsx
+++
@@ -1,5 +0,0 @@
-import { sendTestMail } from "pages/api/send-email";
-export default async function checkUserAPI(request, response) {
-const result = await sendTestMail("test@test.com");
-    response.json(result);
-}
