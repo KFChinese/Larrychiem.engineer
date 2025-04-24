49c22439d2dc4dd1790eb600b1ddd7cc180c3d75
Larry Chiem <larry.chiem@ringcentral.com>
2025-04-24 00:18:23 -0700

    Donzo :c

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-const { spacing, fontFamily } = require('tailwindcss/defaultTheme');
+const { fontFamily } = require('tailwindcss/defaultTheme');
 const colors = require('tailwindcss/colors');
 
 module.exports = {
@@ -8,25 +8,31 @@ module.exports = {
     extend: {
       colors: {
         gray: {
-          ...colors.gray, // THIS pulls in all gray-50 to 900
-          0: '#fff'        // keep your custom shade if needed
+          ...colors.gray,
+          0: '#ffffff', // keep if you need gray.0
         },
         green: {
           701: '#148a06',
-          801: '#106205'
+          801: '#106205',
         },
         'blue-opaque': 'rgb(13 42 148 / 18%)'
       },
       fontFamily: {
-        sans: ['Inter', ...fontFamily.sans]
+        sans: ['Inter', ...fontFamily.sans],
       },
       animation: {
         text: 'text 7s ease infinite'
       },
       keyframes: {
         text: {
-          '0%, 100%': { 'background-size': '200% 200%', 'background-position': 'left center' },
-          '50%': { 'background-size': '200% 200%', 'background-position': 'right center' }
+          '0%, 100%': {
+            'background-size': '200% 200%',
+            'background-position': 'left center'
+          },
+          '50%': {
+            'background-size': '200% 200%',
+            'background-position': 'right center'
+          }
         }
       },
       typography: (theme) => ({
@@ -36,13 +42,15 @@ module.exports = {
             a: {
               color: theme('colors.blue.500'),
               '&:hover': { color: theme('colors.blue.700') },
-              code: { color: theme('colors.blue.400') }
+              code: { color: theme('colors.blue.400') },
             },
             'h2,h3,h4': {
-              'scroll-margin-top': spacing[32]
+              scrollMarginTop: theme('spacing.32'),
             },
-            thead: { borderBottomColor: theme('colors.gray.200') },
-            code: { color: theme('colors.pink.500') }
+            thead: {
+              borderBottomColor: theme('colors.gray.200'),
+            },
+            code: { color: theme('colors.pink.500') },
           }
         },
         dark: {
@@ -51,15 +59,15 @@ module.exports = {
             a: {
               color: theme('colors.blue.400'),
               '&:hover': { color: theme('colors.blue.600') },
-              code: { color: theme('colors.blue.400') }
+              code: { color: theme('colors.blue.400') },
             },
             blockquote: {
               borderLeftColor: theme('colors.gray.700'),
-              color: theme('colors.gray.300')
+              color: theme('colors.gray.300'),
             },
             'h2,h3,h4': {
               color: theme('colors.gray.100'),
-              'scroll-margin-top': spacing[32]
+              scrollMarginTop: theme('spacing.32'),
             },
             hr: { borderColor: theme('colors.gray.700') },
             ol: { li: { '&:before': { color: theme('colors.gray.500') } } },
@@ -67,18 +75,20 @@ module.exports = {
             strong: { color: theme('colors.gray.100') },
             thead: {
               th: { color: theme('colors.gray.100') },
-              borderBottomColor: theme('colors.gray.600')
+              borderBottomColor: theme('colors.gray.600'),
             },
             tbody: {
-              tr: { borderBottomColor: theme('colors.gray.700') }
-            }
+              tr: { borderBottomColor: theme('colors.gray.700') },
+            },
           }
         }
-      })
-    }
+      }),
+    },
   },
   variants: {
-    typography: ['dark']
+    extend: {
+      typography: ['dark'],
+    },
   },
-  plugins: [require('@tailwindcss/typography')]
+  plugins: [require('@tailwindcss/typography')],
 };
