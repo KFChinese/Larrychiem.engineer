172b994c542c6d6b05681e41555bdc156fb6d1ba
Larry Chiem <larry.chiem@ringcentral.com>
2025-04-24 00:43:03 -0700

    Chages to config???

--- next.config.js
+++ next.config.js
@@ -1,13 +1,52 @@
 /**
  * @type {import('next').NextConfig}
  */
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' *.youtube.com *.twitter.com *.google.com kfchinese.github.io;
+  child-src *.youtube.com *.google.com *.twitter.com kfchinese.github.io;
+  style-src 'self' 'unsafe-inline' *.googleapis.com kfchinese.github.io;
+  img-src * blob: data:;
+  media-src 'none';
+  connect-src *;
+  font-src 'self';
+`;
+
+const securityHeaders = [
+  {
+    key: 'Content-Security-Policy',
+    value: ContentSecurityPolicy.replace(/\n/g, '')
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'origin-when-cross-origin'
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'DENY'
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff'
+  },
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on'
+  },
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=31536000; includeSubDomains; preload'
+  },
+  {
+    key: 'Permissions-Policy',
+    value: 'camera=(), microphone=(), geolocation=()'
+  }
+];
+
 module.exports = {
   reactStrictMode: true,
   images: {
     domains: [
-      //'i.scdn.co', // Spotify Album Art
-      //'pbs.twimg.com', // Twitter Profile Picture
-      //'cdn.sanity.io'
       'kfchinese.github.io',
       'raw.githubusercontent.com',
       'avatars2.githubusercontent.com',
@@ -16,81 +55,14 @@ module.exports = {
     ]
   },
   typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
-  
-
-  experimental: {
-    fontLoaders: [
-      { loader: '@next/font/google', options: { subsets: ['latin'] } }
-    ]
+    ignoreBuildErrors: true, // use with caution
   },
   async headers() {
     return [
       {
         source: '/(.*)',
-        headers: securityHeaders,
-        
-
-        
-        
-
-        
+        headers: securityHeaders
       }
     ];
   }
 };
-
-// https://nextjs.org/docs/advanced-features/security-headers
-const ContentSecurityPolicy = `
-    default-src 'self';
-    script-src 'self' 'unsafe-eval' 'unsafe-inline' *.youtube.com *.twitter.com *.google.com kfchinese.github.io;
-    child-src *.youtube.com *.google.com *.twitter.com kfchinese.github.io;
-    style-src 'self' 'unsafe-inline' *.googleapis.com kfchinese.github.io;
-    img-src * blob: data:;
-    media-src 'none';
-    connect-src *;
-    font-src 'self';
-`;
-
-const securityHeaders = [
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
-  {
-    key: 'Content-Security-Policy',
-    value: ContentSecurityPolicy.replace(/\n/g, '')
-  },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
-  {
-    key: 'Referrer-Policy',
-    value: 'origin-when-cross-origin'
-  },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
-  {
-    key: 'X-Frame-Options',
-    value: 'DENY'
-  },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
-  {
-    key: 'X-Content-Type-Options',
-    value: 'nosniff'
-  },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-DNS-Prefetch-Control
-  {
-    key: 'X-DNS-Prefetch-Control',
-    value: 'on'
-  },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
-  {
-    key: 'Strict-Transport-Security',
-    value: 'max-age=31536000; includeSubDomains; preload'
-  },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Feature-Policy
-  {
-    key: 'Permissions-Policy',
-    value: 'camera=(), microphone=(), geolocation=()'
-  }
-];
