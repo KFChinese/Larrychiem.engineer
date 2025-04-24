38ab3fb201cc6b6602fc812725e6c46100def33f
Larry Chiem <larry.chiem@ringcentral.com>
2025-04-23 21:50:01 -0700

    Fix v2 Fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,34 +1,21 @@
 import 'styles/global.css';
-
 import { ThemeProvider } from 'next-themes';
-
 import { Inter } from 'next/font/google';
 
 const inter = Inter({
-  subsets: ['latin'], // ✅ Add this line
-  preload: true,      // Optional but good to be explicit
+  subsets: ['latin'], // ✅ required
+  preload: true,      // optional, true by default
 });
-
-
-
-
-const interVariable = Inter();
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps }
 }) {
   return (
- 
-      <ThemeProvider attribute="class">
-        <main className={interVariable.className}>
-          <Component {...pageProps} />
-        
-        </main>
-
-      </ThemeProvider>
-      
-      
-
+    <ThemeProvider attribute="class">
+      <main className={inter.className}> {/* ✅ use the right one */}
+        <Component {...pageProps} />
+      </main>
+    </ThemeProvider>
   );
 }
