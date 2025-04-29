ae2a44de69e9ea0d1c43b45c5c446c759fee7767
Larry Chiem <larry.chiem@ringcentral.com>
2025-04-29 10:13:21 -0700

    next.js Image Fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -155,7 +155,7 @@ export default function Home() {
           <TechStack
             index="04"
             href="https://github.com/vercel/next.js"
-            img="https://camo.githubusercontent.com/f21f1fa29dfe5e1d0772b0efe2f43eca2f6dc14f2fede8d9cbef4a3a8210c91d/68747470733a2f2f6173736574732e76657263656c2e636f6d2f696d6167652f75706c6f61642f76313636323133303535392f6e6578746a732f49636f6e5f6c696768745f6261636b67726f756e642e706e67"
+            img="https://assets.vercel.com/image/upload/v1662130559/nextjs/Icon_dark_background.png"
             title="Next.js (With TypeScript)"
           />
           <TechStack
